8702c92f311568c91a4a3d99c703140b0b4f860c
sourajitm <52809277+sourajitm@users.noreply.github.com>
2025-07-03 08:46:57 +0530

    Update MyFirstProjectApplication.java

--- MyFirstProject/src/main/java/com/test/demo/MyFirstProjectApplication.java
+++ MyFirstProject/src/main/java/com/test/demo/MyFirstProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyFirstProjectApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello!!!");
 		SpringApplication.run(MyFirstProjectApplication.class, args);
 	}
 
